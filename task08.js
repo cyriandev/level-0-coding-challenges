1a7612a9ac7abc68894e1de831ece27878f62f2f
Cyrian <50519615+cyriandev@users.noreply.github.com>
2021-10-15 15:13:21 +0200

    Update task08.js

--- task08.js
+++ task08.js
@@ -1,16 +1,18 @@
 // TasK 0.8
 
-function toHours(minutes) {
-    hour = minutes / 60;
+function toHours(time) {
+    var hours = time / 60;
 
-    if (hour % 1) {
+    if (hours % 1) {
 
-        var i = hour - Math.floor(hour);
-        var mins = Math.round(i * 60);
-        return `${Math.floor(hour)} hour${Math.floor(hour) > 1 ? "s" : ''}, ${mins} minute${mins > 1 ? "s" : ''}`;
+        var hour = hours - Math.floor(hours);
+        var minutes = Math.round(hour * 60);
+
+        return `${Math.floor(hours)} hour${Math.floor(hours) > 1 ? "s" : ''}, ${minutes} minute${minutes > 1 ? "s" : ''}`;
     }
-    return `${hour} hour${hour > 1 ? "s" : ''}, 0  minutes`;
+    return `${hours} hour${hours > 1 ? "s" : ''}, 0  minutes`;
 
 }
 
 console.log(toHours(71));
+
