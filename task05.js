176b1b726977bac27fdbaa51c34830903ee42be2
Cyrian <50519615+cyriandev@users.noreply.github.com>
2021-10-18 22:07:29 +0200

    update task 5
    removed comments

--- task05.js
+++ task05.js
@@ -3,12 +3,6 @@
 
 function area(a, b, c) {
 
-
-    // Semiperimeter of a triange
-    // s = 1/2 * (a + b + c)
-    // A = Math.sqrt(s(s-a)(s-b)(s-c))
-
-
     var semiperimeter = (a + b + c) / 2;
     var area = Math.sqrt(semiperimeter * (semiperimeter - a) * (semiperimeter - b) * (semiperimeter - c));
 
